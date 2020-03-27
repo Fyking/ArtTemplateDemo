23e9ba46146f7193099bb08c258b3b71c185bb16
fancy <fancy.fang@163.com>
2020-03-27 15:27:21 +0800

    提交代码
    提交代码

---
+++ js/router.js
@@ -0,0 +1,13 @@
+/*基础路由讲解*/
+var route = {
+	page:"index",
+	last:"/",
+	index: {parent: "/",url:""},
+	info: {parent: "index",url:""},
+	detail: {parent: "detail",url:""}
+};
+//获取上级路由结构
+function getRouteParent() {
+
+
+}
